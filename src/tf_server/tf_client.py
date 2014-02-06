6d9dd51a165028a14007ec5cbc07d1e05f599f0e
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-02-06 14:12:24 +0000

    Working of python tf_client
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tf_server@21410 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/tf_server/tf_client.py
+++ src/tf_server/tf_client.py
@@ -4,16 +4,37 @@ import rospy
 
 import tf_server.srv
 
+class TFClient:
+
+    def __init__(self, wait_for_service=True):
+        if wait_for_service:
+            rospy.wait_for_service('tf/lookup_transform')
+        self._srv_lookup_transform = rospy.ServiceProxy('tf/lookup_transform', tf_server.srv.LookupTransform)
+
+    def transformPoint(self, target_frame, point):
+        pass
+        # return point
+
+    def transformPose(self, target_frame, pose):
+        pass
+        # return pose
+
+    def waitForTransform(self, target_frame, source_frame, time, max_duration):    
+        pass
+
+    def lookupTransform(self, target_frame, source_frame, time=rospy.Time(0)):
+        req = tf_server.srv.LookupTransformRequest()
+        req.target_frame = target_frame
+        req.source_frame = source_frame
+        req.time = time
+
+        resp = self._srv_lookup_transform(req)
+
+        if resp.error_msg:
+            rospy.logerr(resp.error_msg)
+        else:
+            return resp.transform.transform.translation, resp.transform.transform.rotation
+
 if __name__ == '__main__':
     rospy.init_node('tf_client')
-
-    rospy.wait_for_service('tf/lookup_transform')
-    srv_lookup_transform = rospy.ServiceProxy('tf/lookup_transform', tf_server.srv.LookupTransform)
-    
-    req = tf_server.srv.LookupTransformRequest()
-    req.target_frame = "/amigo/base_link"
-    req.source_frame = "/amigo/wrist_link6_right"
-
-    resp = srv_lookup_transform(req)
-
-    print resp
+    tf_client = TFClient()
