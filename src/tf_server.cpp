cd64c3b57f6d6f4f0ef0760a45a0aaf639fc0cdb
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-02-06 15:54:53 +0000

    Server should now be hufter-proof
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tf_server@21432 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/tf_server.cpp
+++ src/tf_server.cpp
@@ -12,17 +12,25 @@
 tf::TransformListener* tf_listener_;
 
 bool srvLookupTransform(tf_server::LookupTransform::Request& req, tf_server::LookupTransform::Response& res) {
-    try{
-        tf::StampedTransform t;
+    for(int i = 0; i < 2; ++i) {
+        res.error_msg = "";
+        try{
+            tf::StampedTransform t;
 
-        if (req.fixed_frame == "") {
-            tf_listener_->lookupTransform(req.target_frame, req.source_frame, req.time, t);
-        } else {
-            tf_listener_->lookupTransform(req.target_frame, req.target_time, req.source_frame, req.source_time, req.fixed_frame, t);
+            if (req.fixed_frame == "") {
+                tf_listener_->lookupTransform(req.target_frame, req.source_frame, req.time, t);
+            } else {
+                tf_listener_->lookupTransform(req.target_frame, req.target_time, req.source_frame, req.source_time, req.fixed_frame, t);
+            }
+            tf::transformStampedTFToMsg(t, res.transform);
+            return true;
+        } catch (tf::TransformException& ex){
+            res.error_msg = ex.what();
+            ROS_WARN("Transform error (trying again with zero-time): %s", res.error_msg.c_str());
+
+            // set time to 0-time (= get latest transform) and try again
+            req.time = ros::Time();
         }
-        tf::transformStampedTFToMsg(t, res.transform);
-    } catch (tf::TransformException& ex){
-        res.error_msg = ex.what();        
     }
 
     return true;
@@ -32,17 +40,25 @@ bool srvTransformPoint(tf_server::TransformPoint::Request& req, tf_server::Trans
     tf::Stamped<tf::Point> p_in;
     tf::pointStampedMsgToTF(req.point, p_in);
 
-    try{
-        tf::Stamped<tf::Point> p_out;
+    for(int i = 0; i < 2; ++i) {
+        res.error_msg = "";
+        try{
+            tf::Stamped<tf::Point> p_out;
 
-        if (req.fixed_frame == "") {
-            tf_listener_->transformPoint(req.target_frame, p_in, p_out);
-        } else {
-            tf_listener_->transformPoint(req.target_frame, req.target_time, p_in, req.fixed_frame, p_out);
+            if (req.fixed_frame == "") {
+                tf_listener_->transformPoint(req.target_frame, p_in, p_out);
+            } else {
+                tf_listener_->transformPoint(req.target_frame, req.target_time, p_in, req.fixed_frame, p_out);
+            }
+            tf::pointStampedTFToMsg(p_out, res.point);
+            return true;
+        } catch (tf::TransformException& ex){
+            res.error_msg = ex.what();
+            ROS_WARN("Transform error (trying again with zero-time): %s", res.error_msg.c_str());
+
+            // set time to 0-time (= get latest transform) and try again
+            p_in.stamp_ = ros::Time();
         }
-        tf::pointStampedTFToMsg(p_out, res.point);
-    } catch (tf::TransformException& ex){
-        res.error_msg = ex.what();
     }
 
     return true;
@@ -52,17 +68,25 @@ bool srvTransformPose(tf_server::TransformPose::Request& req, tf_server::Transfo
     tf::Stamped<tf::Pose> p_in;
     tf::poseStampedMsgToTF(req.pose, p_in);
 
-    try{
-        tf::Stamped<tf::Pose> p_out;
+    for(int i = 0; i < 2; ++i) {
+        res.error_msg = "";
+        try{
+            tf::Stamped<tf::Pose> p_out;
 
-        if (req.fixed_frame == "") {
-            tf_listener_->transformPose(req.target_frame, p_in, p_out);
-        } else {
-            tf_listener_->transformPose(req.target_frame, req.target_time, p_in, req.fixed_frame, p_out);
+            if (req.fixed_frame == "") {
+                tf_listener_->transformPose(req.target_frame, p_in, p_out);
+            } else {
+                tf_listener_->transformPose(req.target_frame, req.target_time, p_in, req.fixed_frame, p_out);
+            }
+            tf::poseStampedTFToMsg(p_out, res.pose);
+            return true;
+        } catch (tf::TransformException& ex){
+            res.error_msg = ex.what();
+            ROS_WARN("Transform error (trying again with zero-time): %s", res.error_msg.c_str());
+
+            // set time to 0-time (= get latest transform) and try again
+            p_in.stamp_ = ros::Time();
         }
-        tf::poseStampedTFToMsg(p_out, res.pose);
-    } catch (tf::TransformException& ex){
-        res.error_msg = ex.what();
     }
 
     return true;
