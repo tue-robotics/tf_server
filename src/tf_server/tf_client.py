7d0dcddc1d083530c4dfbb8a0e86ef1700e85802
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-02-06 14:26:10 +0000

    Python interface is complete
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tf_server@21415 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/tf_server/tf_client.py
+++ src/tf_server/tf_client.py
@@ -9,15 +9,40 @@ class TFClient:
     def __init__(self, wait_for_service=True):
         if wait_for_service:
             rospy.wait_for_service('tf/lookup_transform')
+            rospy.wait_for_service('tf/transform_point')
+            rospy.wait_for_service('tf/transform_pose')
+
         self._srv_lookup_transform = rospy.ServiceProxy('tf/lookup_transform', tf_server.srv.LookupTransform)
+        self._srv_transform_point = rospy.ServiceProxy('tf/transform_point', tf_server.srv.TransformPoint)
+        self._srv_transform_pose = rospy.ServiceProxy('tf/transform_pose', tf_server.srv.TransformPose)
 
-    def transformPoint(self, target_frame, point):
-        pass
-        # return point
+    def transformPoint(self, target_frame, point, target_time=rospy.Time(0), fixed_frame=''):
+        req = tf_server.srv.TransformPointRequest()
+        req.point = point
+        req.target_frame = target_frame
+        req.target_time = target_time
+        req.fixed_frame = fixed_frame
 
-    def transformPose(self, target_frame, pose):
-        pass
-        # return pose
+        resp = self._srv_transform_point(req)
+        
+        if resp.error_msg:
+            rospy.logerr(resp.error_msg)
+        else:
+            return resp.point
+
+    def transformPose(self, target_frame, pose, target_time=rospy.Time(0), fixed_frame=''):
+        req = tf_server.srv.TransformPoseRequest()
+        req.pose = pose
+        req.target_frame = target_frame
+        req.target_time = target_time
+        req.fixed_frame = fixed_frame
+
+        resp = self._srv_transform_pose(req)
+        
+        if resp.error_msg:
+            rospy.logerr(resp.error_msg)
+        else:
+            return resp.pose
 
     def waitForTransform(self, target_frame, source_frame, time, max_duration):    
         pass
