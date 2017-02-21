d4abb64088f38c5ae7e7b948aeb087501c5a81d7
Loy van Beek <loy.vanbeek@gmail.com>
2017-02-21 21:03:43 +0100

    Add unittests for transformPoint

--- test/test_client.py
+++ test/test_client.py
@@ -5,9 +5,9 @@ import unittest
 import tf_server
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PointStamped
 
-class TestBasics(unittest.TestCase):
+class TestTfClient(unittest.TestCase):
     def test_transform_pose_1(self):
         target_frame = "/amigo/neck_tilt"
 
@@ -24,6 +24,21 @@ class TestBasics(unittest.TestCase):
         new_pose = tf_client.transformPose(target_frame, ps, timeout=rospy.Time(0.5))
 
         self.assertNotEqual(new_pose.pose.position.z, ps.pose.position.z)  # These two frames are not at the same Z
+
+    def test_transform_point(self):
+        target_frame = "/amigo/neck_tilt"
+
+        tf_client = tf_server.TFClient()
+
+        ps = PointStamped()
+
+        ps.header.frame_id = "/amigo/head_mount"
+        ps.header.stamp = rospy.Time()
+
+        tf_client.waitForTransform(target_frame, ps.header.frame_id, rospy.Time(), rospy.Duration(2.0))
+        new_pose = tf_client.transformPoint(target_frame, ps, timeout=rospy.Time(0.5))
+
+        self.assertNotEqual(new_pose.point.z, ps.point.z)  # These two frames are not at the same Z
 
 
 if __name__ == "__main__":
