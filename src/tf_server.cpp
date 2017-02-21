9f7d16289a48908938b36ee9c536733de48e9e87
Loy van Beek <loy.vanbeek@gmail.com>
2017-02-21 21:00:08 +0100

    Add debug statements that helped me fix this issue

--- src/tf_server.cpp
+++ src/tf_server.cpp
@@ -15,6 +15,7 @@
 tf::TransformListener* tf_listener_;
 
 bool srvLookupTransform(tf_server::LookupTransform::Request& req, tf_server::LookupTransform::Response& res) {
+    ROS_DEBUG("srvLookupTransform(%s, %s, %d, %d)", req.target_frame.c_str(), req.source_frame.c_str(), req.time.toSec(), req.timeout.toSec());
     for(int i = 0; i < 2; ++i) {
         try{
             std::string error_msg;
@@ -100,6 +101,7 @@ bool srvTransformPoint(tf_server::TransformPoint::Request& req, tf_server::Trans
 
 bool srvWaitForTransform(tf_server::WaitForTransform::Request& req, tf_server::WaitForTransform::Response& res) 
 {
+    ROS_DEBUG("srvWaitForTransform(%s, %s, %d, %d)", req.target_frame.c_str(), req.fixed_frame.c_str(), req.target_time.toSec(), req.timeout.toSec());
     try{
         std::string error_msg;
         bool result = tf_listener_->waitForTransform(req.target_frame,
@@ -123,6 +125,7 @@ bool srvWaitForTransform(tf_server::WaitForTransform::Request& req, tf_server::W
 }
 
 bool srvTransformPose(tf_server::TransformPose::Request& req, tf_server::TransformPose::Response& res) {
+    ROS_DEBUG("srvTransformPose(%s, %s, %d, %d)", req.target_frame.c_str(), req.fixed_frame.c_str(), req.target_time.toSec(), req.timeout.toSec());
     tf::Stamped<tf::Pose> p_in;
     tf::poseStampedMsgToTF(req.pose, p_in);
 
