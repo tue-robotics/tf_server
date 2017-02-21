a2fa1fec8bc3e963b7f43891524506bd3cbd0506
Loy van Beek <loy.vanbeek@gmail.com>
2017-02-21 20:58:17 +0100

    The default argument for fixed_frame in TFClient.transformPose is empty and that's wrong

--- test/test_client.py
+++ test/test_client.py
@@ -11,7 +11,7 @@ class TestBasics(unittest.TestCase):
     def test_transform_pose_1(self):
         target_frame = "/amigo/neck_tilt"
 
-        tf_listener = tf_server.TFClient()
+        tf_client = tf_server.TFClient()
 
         ps = PoseStamped()
 
@@ -20,8 +20,8 @@ class TestBasics(unittest.TestCase):
         # We'll leave the position and orientation at zero
         ps.pose.orientation.w = 1
 
-        tf_listener.waitForTransform(target_frame, ps.header.frame_id, rospy.Time(), rospy.Duration(2.0))
-        new_pose = tf_listener.transformPose(target_frame, ps)
+        tf_client.waitForTransform(target_frame, ps.header.frame_id, rospy.Time(), rospy.Duration(2.0))
+        new_pose = tf_client.transformPose(target_frame, ps, timeout=rospy.Time(0.5))
 
         self.assertNotEqual(new_pose.pose.position.z, ps.pose.position.z)  # These two frames are not at the same Z
 
